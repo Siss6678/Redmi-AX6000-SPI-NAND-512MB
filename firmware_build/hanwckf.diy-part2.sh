923252ea681d8b565a7a55a5034e9731742c47c4
Vanco Bashko <86250182+Siss6678@users.noreply.github.com>
2025-03-09 12:14:44 +0200

    Update hanwckf.diy-part2.sh

--- firmware_build/hanwckf.diy-part2.sh
+++ firmware_build/hanwckf.diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
+## let's learn this trick, maybe useful and handy for further Netflix geo-rerouting.
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
